d7036612ea65dc03d88439cd5386b013ea971477
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-30 15:02:34 +0530

    [Tirupathi] Add. Created a contact using constructor

--- AddressBookMain/Program.cs
+++ AddressBookMain/Program.cs
@@ -6,7 +6,8 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program ..!");
+            Console.WriteLine("Welcome to Address Book Program ..!\n");
+            Contact contact = new Contact("Kumamri", "Tirupathi Rao", "Srikakulam", 532221, 0123456789, "tirupathi9436@gmail.com");
         }
     }
 }
